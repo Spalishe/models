51e54f1dc02fcf5475fac6878e235f20f0a687e1
Spalishe <maksamoshkin2010@gmail.com>
2024-06-17 16:47:13 +0300

    br

--- EMAPR/pngToBase64.py
+++ EMAPR/pngToBase64.py
@@ -2,15 +2,10 @@ import base64
 
 Files = [
     "soul_fire_flame.png",
-    "smoke_atlas.png",
     "Lava.png",
     "flash.png",
     "flame.png",
-    "falling_obsidian_tear_atlas.png",
-    "explosion_atlas.png",
-    "end_rod_atlas.png",
     "electric_spark.png",
-    "campfire_smoke_atlas.png",
 ]
 
 for n in Files:
